dd77b79ea175fb3380b06225b849bad477ed8764
Ameer Hamza <61797568+Hamza50537@users.noreply.github.com>
2022-02-11 22:50:33 +0500

    Add files via upload

---
+++ 1-Terraform Basics/ec2-example.tf
@@ -0,0 +1,20 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+# Provider Configuration Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-0a8b4cd432b1c3063" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t2.micro"
+}
